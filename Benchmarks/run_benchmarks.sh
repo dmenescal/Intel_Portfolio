e6d458a649771f70ab44a7f5e05c18757b23f238
dmenescal <dmenescal@outlook.com>
2017-05-05 17:18:53 -0300

    Add files via upload

---
+++ Benchmarks/run_benchmarks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Go to Cholesky folder to run codes
+cd Cholesky
+
+echo "Running Scipy Cholesky code..." 
+python scipy_cholesky.py
+
+echo "Running DAAL Cholesky code..."
+python daal_cholesky.py
+
+# Go back to main folder (Benchmarks)
+cd ..
+
+# Go to Yatch Linear Regression folder to run codes
+cd LinearRegression/YatchLR
+
+echo "Running Scipy LR code..."
+python scipy_LR.py
+
+echo "Running DAAL LR code..."
+python daal_LR.py
+
+# End execution back to main folder
+cd ../..
+
